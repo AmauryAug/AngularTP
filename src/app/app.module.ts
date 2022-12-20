86f0b164e093cd618dbeab14e273925ab79d7bc4
AmauryAug <42046466+AmauryAug@users.noreply.github.com>
2022-12-20 18:42:49 +0100

    Correction TP5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { TachesComponent } from './component/taches/taches.component';
+import { FiltreTachePipe } from './pipe/filtre-tache.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TachesComponent,
+    FiltreTachePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
